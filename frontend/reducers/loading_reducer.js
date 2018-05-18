1172169d65fb25a28458abe8c6e37cdf9dc52f81
Zack Leinwand <aleinwand2020@gmail.com>
2018-05-18 10:52:05 -0400

    remove debuggers

--- frontend/reducers/loading_reducer.js
+++ frontend/reducers/loading_reducer.js
@@ -1,10 +1,8 @@
 import { START_LOADING_POSTS, RECEIVE_POSTS } from '../actions/post_actions'
 
 export default (state = false, action) => {
-  // debugger
   switch (action.type) {
     case START_LOADING_POSTS:
-      // debugger
       return true;
     case RECEIVE_POSTS:
       return false;
